40a289d6022c68e9737fa29a98a3e85ebef1b31b
Ariane Gariso <72839315+Arigariso@users.noreply.github.com>
2020-12-06 12:35:01 -0300

    inner join

--- php/api/produtos.php
+++ php/api/produtos.php
@@ -2,7 +2,11 @@
 
 require_once "../actions/conexao.php";
 
-$result = query("SELECT * FROM produto");
+$result = query("SELECT p.idproduto, c.categoria, p.descricao, p.preco, p.precofinal, p.imagem
+FROM categoria as c
+inner join produto as p
+on p.idproduto = c.produto");
+
 $produtos = [];
 
 while($row = mysqli_fetch_assoc($result)){
